c5a372a57de17b0518634db30523a4b0cfac53fd
Mohanad-Zayan <64075896+Mohanad-Zayan@users.noreply.github.com>
2024-03-18 14:32:10 +0200

    Create migrationEx.ts

---
+++ migrationEx.ts
@@ -0,0 +1,9 @@
+import { QueryInterface, Sequelize } from 'sequelize';
+
+export const up = async (queryInterface: QueryInterface): Promise<void> => {
+  
+};
+
+export const down = async (queryInterface: QueryInterface): Promise<void> => {
+  
+};
